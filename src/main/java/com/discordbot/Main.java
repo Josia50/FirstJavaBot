6b19cdc460af16a8ced2be29d6c62565c74f4dd1
Josia50 <elwyn.vdb@gmail.com>
2018-12-13 20:33:13 +0100

    First

---
+++ src/main/java/com/discordbot/Main.java
@@ -0,0 +1,21 @@
+package com.discordbot;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+
+
+public class Main extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        String token = Config.Token;
+        builder.setToken(token);
+        builder.addEventListener(new Data.Bot());
+        builder.buildAsync();
+        new Data.Bot();
+    }
+}
+
+
